c07bdaecb434912e32eec170b36fdfa8ac012e98
zeph <zephyrzefa15@gmail.com>
2026-02-23 17:07:07 +0100

    sec: removed keys

--- hdk-archive/src/bar/tests.rs
+++ hdk-archive/src/bar/tests.rs
@@ -1,17 +1,3 @@
-/// DEFAULT key used to encrypt BAR file bodies.
-/// Used in BAR archives.
-pub const BAR_DEFAULT_KEY: [u8; 32] = [
-    0x80, 0x6D, 0x79, 0x16, 0x23, 0x42, 0xA1, 0x0E, 0x8F, 0x78, 0x14, 0xD4, 0xF9, 0x94, 0xA2, 0xD1,
-    0x74, 0x13, 0xFC, 0xA8, 0xF6, 0xE0, 0xB8, 0xA4, 0xED, 0xB9, 0xDC, 0x32, 0x7F, 0x8B, 0xA7, 0x11,
-];
-
-/// Signature key used to encrypt BAR file head/signature area.
-/// Used in BAR archives.
-pub const BAR_SIGNATURE_KEY: [u8; 32] = [
-    0xEF, 0x8C, 0x7D, 0xE8, 0xE5, 0xD5, 0xD6, 0x1D, 0x6A, 0xAA, 0x5A, 0xCA, 0xF7, 0xC1, 0x6F, 0xC4,
-    0x5A, 0xFC, 0x59, 0xE4, 0x8F, 0xE6, 0xC5, 0x93, 0x7E, 0xBD, 0xFF, 0xC1, 0xE3, 0x99, 0x9E, 0x62,
-];
-
 #[test]
 fn read() {
     use super::structs::BarArchive;
@@ -22,16 +8,15 @@ fn read() {
     let data_len = data.len() as u32;
     let mut cur = std::io::BufReader::new(std::io::Cursor::new(data));
 
-    let archive =
-        BarArchive::read_le_args(&mut cur, (BAR_DEFAULT_KEY, BAR_SIGNATURE_KEY, data_len))
-            .expect("Failed to read BAR");
+    let archive = BarArchive::read_le_args(&mut cur, ([0u8; 32], [0u8; 32], data_len))
+        .expect("Failed to read BAR");
     println!("Archive: {:#?}", archive);
 
     // Extract all entry data to `../out`
     std::fs::create_dir_all("../out").expect("Failed to create output directory");
     for entry in &archive.entries {
         let data = archive
-            .entry_data(&mut cur, entry, &BAR_DEFAULT_KEY, &BAR_SIGNATURE_KEY)
+            .entry_data(&mut cur, entry, &[0u8; 32], &[0u8; 32])
             .expect("Failed to read entry data");
 
         std::fs::write(format!("../out/{}.bin", entry.name_hash), data)
@@ -44,7 +29,7 @@ fn roundtrip() {
     use super::builder::BarBuilder;
     use crate::structs::CompressionType;
 
-    let builder = BarBuilder::new(BAR_DEFAULT_KEY, BAR_SIGNATURE_KEY);
+    let builder = BarBuilder::new([0u8; 32], [0u8; 32]);
     let mut builder = builder.with_priority(123).with_timestamp(456);
 
     let mut buf = Vec::new();
--- hdk-archive/src/sharc/tests.rs
+++ hdk-archive/src/sharc/tests.rs
@@ -1,13 +1,3 @@
-pub const SHARC_DEFAULT_KEY: [u8; 32] = [
-    0x2F, 0x5C, 0xED, 0xA6, 0x3A, 0x9A, 0x67, 0x2C, 0x03, 0x4C, 0x12, 0xE1, 0xE4, 0x25, 0xFA, 0x81,
-    0x16, 0x16, 0xAE, 0x1C, 0xE6, 0x6D, 0xEB, 0x95, 0xB7, 0xE6, 0xBF, 0x21, 0x40, 0x47, 0x02, 0xDC,
-];
-
-pub const SHARC_SDAT_KEY: [u8; 32] = [
-    0xF1, 0xBF, 0x6A, 0x4F, 0xBB, 0xBA, 0x5D, 0x0E, 0xD2, 0x7F, 0x41, 0x8A, 0x48, 0x88, 0xAF, 0x30,
-    0x47, 0x86, 0xEC, 0xD4, 0x4E, 0x2D, 0x36, 0x46, 0x80, 0xDB, 0x4D, 0xF2, 0x22, 0x3A, 0x9F, 0x56,
-];
-
 #[test]
 fn read() {
     use super::structs::SharcArchive;
@@ -18,8 +8,8 @@ fn read() {
     let data_len = data.len() as u32;
     let mut cur = std::io::BufReader::new(std::io::Cursor::new(data));
 
-    let archive = SharcArchive::read_le_args(&mut cur, (SHARC_DEFAULT_KEY, data_len))
-        .expect("Failed to read SHARC");
+    let archive =
+        SharcArchive::read_le_args(&mut cur, ([0u8; 32], data_len)).expect("Failed to read SHARC");
 
     // Extract all entry data to `../out`
     std::fs::create_dir_all("../out").expect("Failed to create output directory");
@@ -39,7 +29,7 @@ fn roundtrip() {
     use super::builder::SharcBuilder;
     use crate::structs::CompressionType;
 
-    let builder = SharcBuilder::new(SHARC_SDAT_KEY, [0; 16]);
+    let builder = SharcBuilder::new([0u8; 32], [0u8; 16]);
     let mut builder = builder.with_priority(123).with_timestamp(456);
 
     let mut buf = Vec::new();
